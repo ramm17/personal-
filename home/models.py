7b9503f855716e6d6528ed617cfbebfe84cda4ee
Ramm <ramgopal.rana.161@gmail.com>
2022-12-19 20:02:36 +0545

    no message

--- home/models.py
+++ home/models.py
@@ -3,48 +3,48 @@ from django.db import models
 # Create your models here.
 
 class Feedback(models.Model):
-    name=models.CharField(max_length=400)
-    post=models.CharField(max_length=500)
-    comment=models.TextField()
-    image=models.TextField()
+    name = models.CharField(max_length=400)
+    post = models.CharField(max_length=500)
+    comment = models.TextField()
+    image = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Contact(models.Model):
-    name=models.CharField(max_length=150)
-    email=models.EmailField(max_length=200)
-    subject=models.CharField(max_length=400, blank=True)
-    message=models.TextField()
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=400, blank=True)
+    message = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Service(models.Model):
-    name=models.CharField(max_length=150)
-    logo=models.CharField(max_length=300)
-    description=models.TextField()
+    name = models.CharField(max_length=150)
+    logo = models.CharField(max_length=300)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Address(models.Model):
     logo = models.CharField(max_length=300)
-    add=models.CharField(max_length=200)
-    street=models.CharField(max_length=200)
+    add = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
 
     def __str__(self):
         return self.add
 
 class Phone(models.Model):
     logo = models.CharField(max_length=300)
-    phone=models.CharField(max_length=15)
-    working=models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    working = models.CharField(max_length=200)
 
 class Email(models.Model):
     logo = models.CharField(max_length=300)
-    email_address=models.EmailField(max_length=150)
-    query=models.CharField(max_length=200)
+    email_address = models.EmailField(max_length=150)
+    query = models.CharField(max_length=200)
 
 
 
--- home/views.py
+++ home/views.py
@@ -3,28 +3,28 @@ from .models import *
 # Create your views here.
 def home(request):
     views={}
-    views['feedbacks']=Feedback.objects.all()
-    views['contacts']=Contact.objects.all()
-    views['services']=Service.objects.all()
-    views['addresses']=Address.objects.all()
-    views['phones']=Phone.objects.all()
-    views['emails']=Email.objects.all()
-    return render(request,'index.html',views)
-    return render(request, 'contact.html',views)
+    views['feedbacks'] = Feedback.objects.all()
+    views['contacts'] = Contact.objects.all()
+    views['services'] = Service.objects.all()
+    views['addresses'] = Address.objects.all()
+    views['phones'] = Phone.objects.all()
+    views['emails'] = Email.objects.all()
+    return render(request, 'index.html', views)
+    return render(request, 'contact.html', views)
 def about(request):
 
-    return render(request,'about.html')
+    return render(request, 'about.html')
 
 def contact(request):
 
-    return render(request,'contact.html')
+    return render(request, 'contact.html')
 
 def portfolio(request):
-    return render(request,'portfolio.html')
+    return render(request, 'portfolio.html')
 
 def price(request):
 
-    return render(request,'price.html')
+    return render(request, 'price.html')
 
 def services(request):
 
@@ -32,12 +32,12 @@ def services(request):
 
 def blog_home(request):
 
-    return render(request,'blog-home.html')
+    return render(request, 'blog-home.html')
 
 def blog_single(request):
 
-    return render(request,'blog-single.html')
+    return render(request, 'blog-single.html')
 
 def elements(request):
 
-    return render(request,'elements.html')
+    return render(request, 'elements.html')
